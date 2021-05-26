a94e4925bf3fdc0153fa963ed386aa27569a8496
Rajnish Sahu <rajnishsahu95@gmail.com>
2021-05-26 23:05:20 +0530

    Validate First Name

--- src/main/java/RegistrationForm.java
+++ src/main/java/RegistrationForm.java
@@ -3,11 +3,23 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegistrationForm {
+    boolean validateFirstName(String firstName) {
+        String regex = "[A-Z][a-z]*";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(firstName);
+        return matcher.matches();
+    }
 
 
     public static void main(String[] args) {
         System.out.println("User Registration");
         System.out.println("Enter Details");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter First Name");
+        String firstName = scanner.nextLine();
+        RegistrationForm registrationForm = new RegistrationForm();
+        System.out.println(registrationForm.validateFirstName(firstName));
 
     }
 }
