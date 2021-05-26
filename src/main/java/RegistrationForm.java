ab49fbfc01c070d41b2e172a6f44d1fa5cfc1246
Rajnish Sahu <rajnishsahu95@gmail.com>
2021-05-26 23:21:18 +0530

    Validate user mobile number

--- src/main/java/RegistrationForm.java
+++ src/main/java/RegistrationForm.java
@@ -4,16 +4,23 @@ import java.util.regex.Pattern;
 
 public class RegistrationForm {
     boolean validateFirstName(String firstName) {
-        String regex = "[A-Z][a-z]*";
+        String regex = "[A-Z][a-z]{3,}";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(firstName);
         return matcher.matches();
     }
 
     boolean validateLastName(String lastName) {
-        String regex = "[A-Z][a-z]*";
+        String regex = "[A-Z][a-z]{3,}";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(lastName);
+        return matcher.matches();
+    }
+
+    boolean validateMobileNo(String mobileNo) {
+        String regex = "\\d{2} \\d{10}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(mobileNo);
         return matcher.matches();
     }
 
@@ -23,14 +30,19 @@ public class RegistrationForm {
         System.out.println("Enter Details");
         Scanner scanner = new Scanner(System.in);
 
+        RegistrationForm registrationForm = new RegistrationForm();
+        
         System.out.println("Enter First Name");
         String firstName = scanner.nextLine();
-        RegistrationForm registrationForm = new RegistrationForm();
         System.out.println(registrationForm.validateFirstName(firstName));
 
         System.out.println("Enter Last Name");
         String lastName = scanner.nextLine();
         System.out.println(registrationForm.validateLastName(lastName));
+
+        System.out.println("Enter Mobile No :");
+        String mobileNo = scanner.nextLine();
+        System.out.println(registrationForm.validateMobileNo(mobileNo));
 
 
     }
