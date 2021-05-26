1823dd98c88d36f85f96042ad33c0c48c437f214
Rajnish Sahu <rajnishsahu95@gmail.com>
2021-05-26 23:02:36 +0530

    Welcome to User Registraion

---
+++ src/main/java/RegistrationForm.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegistrationForm {
+
+
+    public static void main(String[] args) {
+        System.out.println("User Registration");
+        System.out.println("Enter Details");
+
+    }
+}
