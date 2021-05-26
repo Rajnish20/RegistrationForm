c49e202926f083055d598cce5b19213ef5c87c33
Rajnish Sahu <rajnishsahu95@gmail.com>
2021-05-26 23:07:33 +0530

    Validate Last Name

--- src/main/java/RegistrationForm.java
+++ src/main/java/RegistrationForm.java
@@ -10,16 +10,28 @@ public class RegistrationForm {
         return matcher.matches();
     }
 
+    boolean validateLastName(String lastName) {
+        String regex = "[A-Z][a-z]*";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(lastName);
+        return matcher.matches();
+    }
+
 
     public static void main(String[] args) {
         System.out.println("User Registration");
         System.out.println("Enter Details");
         Scanner scanner = new Scanner(System.in);
-        
+
         System.out.println("Enter First Name");
         String firstName = scanner.nextLine();
         RegistrationForm registrationForm = new RegistrationForm();
         System.out.println(registrationForm.validateFirstName(firstName));
+
+        System.out.println("Enter Last Name");
+        String lastName = scanner.nextLine();
+        System.out.println(registrationForm.validateLastName(lastName));
+
 
     }
 }
